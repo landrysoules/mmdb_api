464a62dd467753b154e407ab88a4c730b1db475c
Landry Soules <landry@spaceinvade.rs>
2017-07-29 10:15:14 +0200

    Fix mongoid error with semaphoreci

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Mongoid.load!('config/mongoid.yml')
 module MmdbApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
